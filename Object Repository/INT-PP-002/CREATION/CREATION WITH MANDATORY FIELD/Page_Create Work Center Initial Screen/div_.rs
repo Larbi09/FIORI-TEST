3664fbdabfce3d1f747202a6a8502fd9f89ea8fc
arbi.benzarti <arbi.benzarti@CORPTN-LAP-886.coficab.com>
2024-05-13 17:18:43 +0200

    TC

---
+++ Object Repository/INT-PP-002/CREATION/CREATION WITH MANDATORY FIELD/Page_Create Work Center Initial Screen/div_.rs
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_</name>
+   <tag></tag>
+   <elementGuidId>40aefc3e-74d4-464e-bc28-4e856eab8786</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//td[@id='sh[2,0]#sc']/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.urBorderBox.lsSapTable--expandWidth.lsSTCellHeight100.urSTSCOuterDiv.urSTRowSelSecIcon.urST4LbSelIcon > div.urST5SCMetricInner.lsSapTable--expandHeight</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>#SHresultgrid1-mrss-cont-left-Row-1 div >> internal:has-text=&quot;Row selected. To deselect the row, press CTRL and the spacebar.&quot;i >> div</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>c1bcb6b0-92d7-4209-996e-d1ee8ac9057a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>-1</value>
+      <webElementGuid>e686aa85-ec2b-4bfa-87d8-ca129971c5b4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ti</name>
+      <type>Main</type>
+      <value>-1</value>
+      <webElementGuid>cf7cdff4-b7f5-44a0-a009-aa2e77ce0efe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>urST5SCMetricInner lsSapTable--expandHeight</value>
+      <webElementGuid>926379da-5992-4914-991e-18dd16dbe021</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> </value>
+      <webElementGuid>076f9992-b135-4f47-af39-44e141109dee</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;sh[2,0]#sc&quot;)/div[@class=&quot;urBorderBox lsSapTable--expandWidth lsSTCellHeight100 urSTSCOuterDiv urSTRowSelSecIcon urST4LbSelIcon&quot;]/div[@class=&quot;urST5SCMetricInner lsSapTable--expandHeight&quot;]</value>
+      <webElementGuid>aa14478b-82b3-4da8-a1c0-aed512691834</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/INT-PP-002/CREATION/CREATION WITH MANDATORY FIELD/Page_Create Work Center Initial Screen/iframe_Drag apps here to create links_appli_6322ab</value>
+      <webElementGuid>350a459f-2e47-44fe-8040-87a5967e73f0</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//td[@id='sh[2,0]#sc']/div/div</value>
+      <webElementGuid>1841a6e7-4386-4eda-a9ef-6cf09bf81bdf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='To select a row, press the spacebar.'])[1]/following::div[2]</value>
+      <webElementGuid>9fffa799-cb85-4bd0-a52e-9c45295a8d9b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Version'])[1]/following::div[9]</value>
+      <webElementGuid>b05cc98a-f244-4035-8ec1-d466f76c3af0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Row selected. To deselect the row, press CTRL and the spacebar.'])[1]/preceding::div[1]</value>
+      <webElementGuid>b72663da-c02e-4c0d-8f4b-5725152c124d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='To select a row, press the spacebar.'])[2]/preceding::div[2]</value>
+      <webElementGuid>45b3a487-a44d-4e26-80f5-ebbdf1c0967e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//td/div/div/table/tbody/tr[2]/td/div/div</value>
+      <webElementGuid>8d9eb658-7603-44f2-b7a9-f1aa600760ea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = ' ' or . = ' ')]</value>
+      <webElementGuid>76ba4aae-0ded-4e1e-baa4-8c29395fd3d1</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
