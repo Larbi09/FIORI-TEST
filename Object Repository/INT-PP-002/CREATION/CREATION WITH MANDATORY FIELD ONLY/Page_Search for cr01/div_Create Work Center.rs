fa9f70c1976f06b991c9d90ddd2846fa8ee233cf
arbi.benzarti <arbi.benzarti@CORPTN-LAP-886.coficab.com>
2024-05-13 15:29:08 +0200

    FIRST TC

---
+++ Object Repository/INT-PP-002/CREATION/CREATION WITH MANDATORY FIELD ONLY/Page_Search for cr01/div_Create Work Center.rs
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Create Work Center</name>
+   <tag></tag>
+   <elementGuidId>f06d609a-6d41-4b36-8eca-0dee6fe8ad42</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='__tile587']/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#__tile587 > div.sapMGTHdrContent.OneByOne</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:label=&quot;Create Work Center\nTile&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>4a286007-0bf3-4f1a-8e45-8ac0f0b717be</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sapMGTHdrContent OneByOne</value>
+      <webElementGuid>87240159-19a1-47d2-8628-9c4aa2f905ff</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Create Work Center</value>
+      <webElementGuid>20305ebf-71fc-4c0b-bf6c-cd1d6fcb32cb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;__tile587&quot;)/div[@class=&quot;sapMGTHdrContent OneByOne&quot;]</value>
+      <webElementGuid>3230f3a2-a00b-4a52-a22d-b6b1ed26bb2c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//a[@id='__tile587']/div</value>
+      <webElementGuid>2d4bb04d-8cd7-4409-9c2c-42f1524076ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Results (1)'])[1]/following::div[11]</value>
+      <webElementGuid>7e3d2d42-59a4-4e6c-b56c-28bddcfda358</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apps'])[2]/following::div[26]</value>
+      <webElementGuid>d9e50bd6-d8f7-45cf-9885-63f2dc2ef0b7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/a/div</value>
+      <webElementGuid>d625ad48-eb01-4648-9fde-e0275132a3f4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Create Work Center' or . = 'Create Work Center')]</value>
+      <webElementGuid>37a9d709-856c-4047-9ae7-0d566909ba10</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
